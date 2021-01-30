dc05f3d66db501c7a6f3330562aaf7ddd1c31c3b
Khamarl C <khamarlchristie@hotmail.co.uk>
2021-01-30 13:29:11 +0000

    arrays added

---
+++ random.js
@@ -0,0 +1,8 @@
+
+// arrays of ingredients for random smoothie to put into an object
+const fruit = ['apple','pineapple','mango', 'cherries', 'banana', 'strawberries'];
+const veg = ['spinach', 'sprulina', 'kale', 'celery', 'carrots', 'cucumber'];
+const liquid = ['water', 'yoghurt', 'milk', 'oat milk', 'soy milk', 'orange juice'];
+const extra = ['flax seeds', 'chia seeds', 'protein powder', 'ginger', 'turmeric', 'peanuts']
+
+// Final sentence 
